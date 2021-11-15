33f23f6af05ffbd4ded180591e44cc87bdd8a442
Febri Wijaya <tampuboloncarles21@gmail.com>
2021-11-15 10:20:56 +0700

    Add Heroku deployment files

--- sesi9/config.py
+++ sesi9/config.py
@@ -13,7 +13,7 @@ app = connex_app.app
 
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'people.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ondiwxvvbhkdpv:6bdf2c9942c525bc1ff5c414091a9ddb18dff3402b1e4143b12fa27e71bf2d47@ec2-52-204-72-14.compute-1.amazonaws.com:5432/d1j1521qa77p4m'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
