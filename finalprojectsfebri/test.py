43adc078348b1131529612de82ff0d6d24573f06
febri wijaya <59653659+febriwijaya@users.noreply.github.com>
2021-11-18 07:39:47 +0700

    Update test.py

--- finalprojectsfebri/test.py
+++ finalprojectsfebri/test.py
@@ -1,18 +1,21 @@
 import unittest
-
-from directors import read_all
-from movies import read_all
+from directors import *
+from app import connex_app
 
 class TestDirectors(unittest.TestCase):
 
-    def test_read_all(self):
-        self.assertIs(type(read_all()), list)
+  def test_read_all(self):
+    self.assertIs(type(read_all()), list)
 
-class TestMovies(unittest.TestCase):
-  
-    def test_read_all(self):
-        self.assertIs(type(read_all()), list)
+  def test_read_one(self):
+    self.assertIs(type(read_one(4770)), dict)
 
+class FlaskTestCase(unittest.TestCase):
+  def test_get_all_directors(self):
+    connex_app.app.testing = True
+    tester = connex_app.app.test_client(self)
+    response = tester.get('/api/directors')
+    self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
